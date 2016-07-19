bf4805897c3db492e6bbaeb64f81d8045f1edd36
amitkp9 <amitkumar_p@hotmail.co.uk>
2016-07-19 08:52:34 +0100

    Add files via upload

---
+++ car.java
@@ -0,0 +1,80 @@
+
+
+public class Car
+{
+private square engine;
+private square window,
+private square frontWheel;
+private square backWheel;
+private circle alarm;
+
+public Car (engine e; square w; square fw;  square bw, circle a)
+
+super();
+this.engine = e;
+this.window = w;
+this.frontWheel = fw;
+this.backWheel = bw;
+this.alarm = a;
+
+private void setInitialEnginePos()
+
+{
+
+this.getEngine().setColour()
+this.getEngine().setLength()
+this.getEngine().setYPos()
+this.getEngine().setXPos()
+}
+
+private void setInitialWindowPos()
+
+{
+	
+this.getWindow().setColour()	
+this.getEngine().setLength()
+this.getEngine().setYPos()
+this.getEngine().setXPos()	
+	
+}
+
+private void setInitialFrontWheelPos()
+
+(
+
+this.getFrontWheel().setDiameter();
+this.getFrontWheel().setXpos();
+this.getFrontWheel().setYPos();
+
+)
+
+private void setInitialFrontWheelPos()
+
+(
+
+this.getFrontWheel().setDiameter();
+this.getFrontWheel().setXpos();
+this.getFrontWheel().setYPos();
+
+
+)
+
+private void setInitialAlarmPos()
+{
+
+this.getAlarm().setColour()	
+this.getAlarm().setDiameter()
+this.getAlarm().setYPos()
+this.getAlarm().setXPos()	
+}
+
+private square getEngine()
+
+{
+	
+	return this.engine;
+}
+}
+
+}
+
